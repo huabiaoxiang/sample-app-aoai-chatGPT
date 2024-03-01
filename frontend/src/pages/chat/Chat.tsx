9f253a55f7a3a3ea83ba164ce22f4f25a579eaa9
huabiaoxiang <161795293+huabiaoxiang@users.noreply.github.com>
2024-03-01 12:04:36 +0800

    Update Chat.tsx
    not auth

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -100,9 +100,9 @@ const Chat = () => {
     }, [appStateContext?.state.chatHistoryLoadingState])
 
     const getUserInfoList = async () => {
-        if (!AUTH_ENABLED) {
+        //if (!AUTH_ENABLED) {
             setShowAuthMessage(false);
-            return;
+        //    return;
         }
         const userInfoList = await getUserInfo();
         if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
